85896e5d7e3ee27a95c9b102c0fb9c028e1ffaa1
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2024-03-13 20:46:56 -0300

    [GKO-7] Configurando CORS

--- src/main/java/com/allankardeconline/Application.java
+++ src/main/java/com/allankardeconline/Application.java
@@ -2,13 +2,23 @@ package com.allankardeconline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
 
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/gk/curso").allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 
 }
