a2629814fc6469af2e6dcc84a4a8b7167c40a081
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2024-07-21 21:07:14 -0300

    [GKO-40] Iniciando desenvolvimento da tratativa dos erros de usuário e pessoa.

--- src/main/java/com/allankardeconline/globalkardec/controller/PessoaController.java
+++ src/main/java/com/allankardeconline/globalkardec/controller/PessoaController.java
@@ -36,109 +36,109 @@ import jakarta.validation.Valid;
 @Tag(name = "Pessoa", description = "Endpoints para gerenciar pessoas")
 public class PessoaController {
 
-	@Autowired
-	private PessoaService service;
+    @Autowired
+    private PessoaService service;
 
-	@GetMapping(value = "/{uuid}", produces = MediaType.APPLICATION_JSON_VALUE)
-	@Operation(summary = "Pessoa por uuid", tags = {
-			"Pessoa"
-	}, responses = {
-			@ApiResponse
-	})
-	public PessoaDTO obterPorUuid(@PathVariable UUID uuid) {
-		return service.obterPorUuid(uuid);
-	}
+    @GetMapping(value = "/{uuid}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Pessoa por uuid", tags = {
+            "Pessoa"
+    }, responses = {
+            @ApiResponse
+    })
+    public PessoaDTO obterPorUuid(@PathVariable UUID uuid) {
+        return service.obterPorUuid(uuid);
+    }
 
-	@GetMapping(value = "/porNome", produces = MediaType.APPLICATION_JSON_VALUE)
-	@Operation(summary = "Lista de pessoas por nome", tags = {
-			"Pessoa"
-	}, responses = {
-			@ApiResponse
-	})
-	public ResponseEntity<Page<PessoaDTO>> obterPorNome(
-			@RequestParam(name = "nome", defaultValue = "") String nome,
-			@RequestParam(value = "pagina", defaultValue = "0") Integer pagina,
-			@RequestParam(value = "limite", defaultValue = "10") Integer limite) {
+    @GetMapping(value = "/porNome", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Lista de pessoas por nome", tags = {
+            "Pessoa"
+    }, responses = {
+            @ApiResponse
+    })
+    public ResponseEntity<Page<PessoaDTO>> obterPorNome(
+            @RequestParam(name = "nome", defaultValue = "") String nome,
+            @RequestParam(value = "pagina", defaultValue = "0") Integer pagina,
+            @RequestParam(value = "limite", defaultValue = "10") Integer limite) {
 
-		Pageable paginacao = PageRequest.of(pagina, limite);
+        Pageable paginacao = PageRequest.of(pagina, limite);
 
-		return ResponseEntity.ok(service.obterPorNome(nome, paginacao));
-	}
+        return ResponseEntity.ok(service.obterPorNome(nome, paginacao));
+    }
 
-	@GetMapping(value = "/porEmailOuTelefone", produces = MediaType.APPLICATION_JSON_VALUE)
-	@Operation(summary = "Lista de pessoas por E-mail OU Telefone", tags = {
-			"Pessoa"
-	}, responses = {
-			@ApiResponse
-	})
-	public List<PessoaDTO> obterPorTelefoneOuEmail(
-			@RequestParam(name = "email", defaultValue = "") String email,
-			@RequestParam(name = "telefone", defaultValue = "") String telefone) {
+    @GetMapping(value = "/porEmailOuTelefone", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Lista de pessoas por E-mail OU Telefone", tags = {
+            "Pessoa"
+    }, responses = {
+            @ApiResponse
+    })
+    public List<PessoaDTO> obterPorTelefoneOuEmail(
+            @RequestParam(name = "email", defaultValue = "") String email,
+            @RequestParam(name = "telefone", defaultValue = "") String telefone) {
 
-		return service.obterPorEmailOUTelefone(email, telefone);
-	}
+        return service.obterPorEmailOUTelefone(email, telefone);
+    }
 
-	@GetMapping(value = "/porCentro/{uuidCentro}/porNomeOuEmailOuTelefone", produces = MediaType.APPLICATION_JSON_VALUE)
-	@Operation(summary = "Lista de pessoas de um centro filtradas por Nome E/OU E-mail E/OU Telefone", tags = {
-			"Pessoa"
-	}, responses = {
-			@ApiResponse
-	})
-	public List<PessoaConsultaDTO> obterPorCentroENomeOUEmailOUTelefone(
-			@PathVariable UUID uuidCentro,
-			@RequestParam(name = "campoBusca", defaultValue = "") String campoBusca) {
+    @GetMapping(value = "/porCentro/{uuidCentro}/porNomeOuEmailOuTelefone", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Lista de pessoas de um centro filtradas por Nome E/OU E-mail E/OU Telefone", tags = {
+            "Pessoa"
+    }, responses = {
+            @ApiResponse
+    })
+    public List<PessoaConsultaDTO> obterPorCentroENomeOUEmailOUTelefone(
+            @PathVariable UUID uuidCentro,
+            @RequestParam(name = "campoBusca", defaultValue = "") String campoBusca) {
 
-		return service.obterPorCentroENomeOUEmailOUTelefone(uuidCentro,
-				campoBusca);
-	}
+        return service.obterPorCentroENomeOUEmailOUTelefone(uuidCentro,
+                campoBusca);
+    }
 
-	@GetMapping(value = "/porCentro/{uuidCentro}", produces = MediaType.APPLICATION_JSON_VALUE)
-	@Operation(summary = "Todas as pessoas por centro", tags = {
-			"Pessoa"
-	}, responses = {
-			@ApiResponse
-	})
-	public ResponseEntity<Page<PessoaDTO>> obterTodos(
-			@PathVariable UUID uuidCentro,
-			@RequestParam(value = "pagina", defaultValue = "0") Integer pagina,
-			@RequestParam(value = "limite", defaultValue = "10") Integer limite) {
+    @GetMapping(value = "/porCentro/{uuidCentro}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Todas as pessoas por centro", tags = {
+            "Pessoa"
+    }, responses = {
+            @ApiResponse
+    })
+    public ResponseEntity<Page<PessoaDTO>> obterTodos(
+            @PathVariable UUID uuidCentro,
+            @RequestParam(value = "pagina", defaultValue = "0") Integer pagina,
+            @RequestParam(value = "limite", defaultValue = "10") Integer limite) {
 
-		Pageable paginacao = PageRequest.of(pagina, limite);
+        Pageable paginacao = PageRequest.of(pagina, limite);
 
-		return ResponseEntity.ok(service.obterTodos(paginacao));
-	}
+        return ResponseEntity.ok(service.obterTodos(paginacao));
+    }
 
-	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	@Operation(summary = "Inserir uma nova pessoa", tags = {
-			"Pessoa"
-	}, responses = {
-			@ApiResponse
-	})
-	@ResponseStatus(HttpStatus.CREATED)
-	public PessoaDTO inserir(@RequestBody @Valid PessoaCadastroDTO pessoa) {
-		return service.inserir(pessoa);
-	}
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Inserir uma nova pessoa", tags = {
+            "Pessoa"
+    }, responses = {
+            @ApiResponse
+    })
+    @ResponseStatus(HttpStatus.CREATED)
+    public PessoaDTO inserir(@RequestBody @Valid PessoaCadastroDTO pessoa) {
+        return service.inserir(pessoa);
+    }
 
-	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	@Operation(summary = "Atualizar dados de uma pessoa", tags = {
-			"Pessoa"
-	}, responses = {
-			@ApiResponse
-	})
-	public PessoaDTO alterar(@RequestBody PessoaCadastroDTO pessoa) {
-		return service.alterar(pessoa);
-	}
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Atualizar dados de uma pessoa", tags = {
+            "Pessoa"
+    }, responses = {
+            @ApiResponse
+    })
+    public PessoaDTO alterar(@RequestBody PessoaCadastroDTO pessoa) {
+        return service.alterar(pessoa);
+    }
 
-	@DeleteMapping(value = "/{uuid}")
-	@Operation(summary = "Permite a exclusão de uma pessoa por uuid", tags = {
-			"Pessoa"
-	}, responses = {
-			@ApiResponse
-	})
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public ResponseEntity<?> excluir(@PathVariable UUID uuid) {
-		service.excluir(uuid);
-		return ResponseEntity.noContent().build();
-	}
+    @DeleteMapping(value = "/{uuid}")
+    @Operation(summary = "Permite a exclusão de uma pessoa por uuid", tags = {
+            "Pessoa"
+    }, responses = {
+            @ApiResponse
+    })
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<?> excluir(@PathVariable UUID uuid) {
+        service.excluir(uuid);
+        return ResponseEntity.noContent().build();
+    }
 
 }
